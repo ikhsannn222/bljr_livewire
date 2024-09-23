e09e42778b96f8626110a71783952b6361ab7cdd
unknown <m26ikhsan26maulana@gmail.com>
2024-09-23 13:07:25 +0700

    bljr_livewire

---
+++ app/Livewire/Posts/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Posts;
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+#[Title('posts')]
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.posts.index');
+    }
+}
